a7ac4a881c60e04083442925a6781a0ed7665d42
LiXiang <xianglqqo@gmail.com>
2019-05-08 17:23:42 +0800

    将port配置项转移到components作用域内

--- app/http.js
+++ app/http.js
@@ -1,25 +1,20 @@
 'use strict';
 
-const ioa = require('ioa');
+const app = require('@app');
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const routerMiddleware = require('ioa-router');
+const logger = require('loggercc');
 
-const { main, logger } = ioa;
+const { config } = app;
 
 let port = process.env.PORT || 8800;
-
-const { config } = main;
 
 if (config && config.port) {
    port = config.port;
 }
 
-ioa.port = port;
-
 const koa = new Koa();
-
-ioa.koa = koa;
 
 koa.use(bodyParser());
 koa.use(routerMiddleware);
---
+++ main/config/default.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+   'components': {
+      "./": {
+         "port": 8600
+      }
+   },
+}
