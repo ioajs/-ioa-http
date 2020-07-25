0c8d4eee376ba3dda4cd60b00daa5eab655e6cfc
lixiang <lixiangio@qq.com>
2020-07-25 20:04:43 +0800

    9.8.2

--- main/config/default.js
+++ main/config/default.js
@@ -3,6 +3,7 @@
 module.exports = {
    "lib": {
       "port": 8600,
+      "debug": true,
       "parser": {
          enableTypes: ['json', 'form', 'text'],
          extendTypes: { text: ['text/xml', 'application/xml'] }
